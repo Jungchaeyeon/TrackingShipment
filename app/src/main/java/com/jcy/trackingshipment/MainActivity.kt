cf066c348baf988965856a46fe3df1ebbbab1ad0
Jungchaeyeon <chaeyeon1697@gmail.com>
2021-10-06 12:23:25 +0900

    프로젝트 셋업

---
+++ app/src/main/java/com/jcy/trackingshipment/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.jcy.trackingshipment
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
